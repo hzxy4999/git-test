d7a9f56df6c816af49d99cf633450d8fb2d45b9c
Edwin <Edwin@ediwn.com>
2022-03-11 10:37:56 +0800

    from idea commit

---
+++ src/main/java/com/study/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.study.git;
+
+/**
+ * @author: Hzxy
+ * @date: 2022/3/11 10:34
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+    }
+}

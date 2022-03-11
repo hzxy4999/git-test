4789dc2e52ce7fa0b9cfabae11a6d8274a09463a
Edwin <Edwin@ediwn.com>
2022-03-11 10:40:02 +0800

    second idea commit

--- src/main/java/com/study/git/GitTest.java
+++ src/main/java/com/study/git/GitTest.java
@@ -8,5 +8,6 @@ package com.study.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git ");
+        System.out.println("hello git 2222222222");
     }
 }

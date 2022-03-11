2d4bcad38177482b9814dc2172b2ec26a5a302b7
Edwin <Edwin@ediwn.com>
2022-03-11 10:55:22 +0800

    hot-fix idea commit

--- src/main/java/com/study/git/GitTest.java
+++ src/main/java/com/study/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git ");
         System.out.println("hello git 2222222222");
         System.out.println("hello git 3333333333");
+        System.out.println("hello git 444444444444");
     }
 }
